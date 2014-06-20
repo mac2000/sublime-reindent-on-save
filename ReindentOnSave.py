dcac3728797da3d6ce8f058c04fb953dbeb14979
Marchenko Alexandr <marchenko.alexandr@gmail.com>
2014-06-20 16:43:00 +0300

    kiss

--- ReindentOnSave.py
+++ ReindentOnSave.py
@@ -2,17 +2,4 @@ import sublime, sublime_plugin, os
 
 class ReindentOnSave(sublime_plugin.EventListener):
   def on_pre_save(self, view):
-    # Save current selections for restore
-    sels = []
-    for sel in view.sel(): sels.append(sel)
-
-    # Select All
-    view.sel().clear()
-    view.sel().add(sublime.Region(0, view.size()))
-
-    # Reindent
-    view.window().run_command('reindent')
-
-    # Restore saved selections
-    view.sel().clear()
-    view.sel().add_all(sels)
+    view.window().run_command('reindent', {"single_line": False})
